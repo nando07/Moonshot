8edbe5d656802795b7935ab2caf597933735c71e
Fernando Gomez <redchameleonapps@gmail.com>
2023-02-14 22:46:52 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Fernando Gomez on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
