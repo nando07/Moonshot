14c576a21f2742fb08f0ed2ea59543032b3e256a
Fernando Gomez <redchameleonapps@gmail.com>
2023-02-16 18:24:19 -0700

    Added images and JSON data

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Fernando Gomez on 2/16/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,39 +7,14 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    let text: String
-
-    var body: some View {
-        Text(text)
-    }
-
-    init(_ text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
-}
 
 struct ContentView: View {
+    
+    let astronauts = Bundle.main.decode("astronauts.json")
+    
     var body: some View {
-        
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText("Item \($0)")
-                        .font(.title)
-                }
-            }
-            .frame(maxWidth: .infinity)
-        }
-        
-//        GeometryReader { geo in
-//            Image("landscape")
-//                .resizable()
-//                .scaledToFit()
-//                .frame(width: geo.size.width * 0.8)
-//                .frame(width: geo.size.width, height: geo.size.height)
-//        }
+        Text("\(astronauts.count)")
+  
     }
 }
 
