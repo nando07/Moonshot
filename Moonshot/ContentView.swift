ea46cf0efa5b4e12fb939fa9b4b8534b8cbd8284
Fernando Gomez <redchameleonapps@gmail.com>
2023-02-22 22:41:54 -0700

    Added two more views.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,29 +9,33 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    
-    let columns = [GridItem(.adaptive(minimum: 150))]
-    
+
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+
     var body: some View {
         NavigationView {
             ScrollView {
                 LazyVGrid(columns: columns) {
                     ForEach(missions) { mission in
                         NavigationLink {
-                            Text("Detail view")
+                            MissionView(mission: mission, astronauts: astronauts)
                         } label: {
                             VStack {
                                 Image(mission.image)
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
+                                    .padding()
+
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
                                         .foregroundColor(.white)
+
                                     Text(mission.formattedLaunchDate)
                                         .font(.caption)
                                         .foregroundColor(.white.opacity(0.5))
@@ -42,10 +46,10 @@ struct ContentView: View {
                             }
                             .clipShape(RoundedRectangle(cornerRadius: 10))
                             .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(.lightBackground))
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
                         }
-                        
                     }
                 }
                 .padding([.horizontal, .bottom])
